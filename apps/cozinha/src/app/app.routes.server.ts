91f87035627df007e75a6b1ac779c2b2b00735c8
Vanessa Ortega <ortegavan@live.com>
2026-08-23 21:56:28 -0300

    chore: remove a aplicação para configurar monorepo antes

--- apps/cozinha/src/app/app.routes.server.ts
+++
@@ -1,9 +0,0 @@
-import { RenderMode } from '@angular/ssr';
-import type { ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender,
-  },
-];
